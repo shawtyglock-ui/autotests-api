3ae4f5805a1230af7422ee18459042fd6a9d6d82
shawtyglock-ui <sahatiy13@gmail.com>
2026-03-03 19:03:02 +0300

    8.6 Плагины Pytest

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,29 +1,33 @@
 from http import HTTPStatus
 
-from clients.authentication.authentication_client import get_authentication_client
+import pytest
+
+from clients.authentication.authentication_client import AuthenticationClient
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
-from clients.users.public_users_client import get_public_users_client
-from clients.users.users_schema import CreateUserRequestSchema
-from tools.assertions.base import assert_status_code
+from clients.users.public_users_client import PublicUsersClient
+from fixtures.users import UserFixture  # Заменяем импорт
 from tools.assertions.authentication import assert_login_response
+from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
+# Остальной код без изменений
 
-def test_login():
-    public_users_client = get_public_users_client()
-    authentication_client = get_authentication_client()
 
-    create_user_request = CreateUserRequestSchema()
-    public_users_client.create_user(create_user_request)
+@pytest.mark.regression
+@pytest.mark.authentication
+def test_login(
+        function_user: UserFixture,  # Используем фикстуру для создания пользователя
+        authentication_client: AuthenticationClient
+):
+    # Запрос на логин (login_request -> request)
+    request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+    # Выполняем логин (login_response -> response)
+    response = authentication_client.login_api(request)
+    # Валидация ответа (login_response_data -> response_data)
+    response_data = LoginResponseSchema.model_validate_json(response.text)
 
-    login_request = LoginRequestSchema(
-        email=create_user_request.email,
-        password=create_user_request.password
-    )
-    login_response = authentication_client.login_api(login_request)
-    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_login_response(response_data)
 
-    assert_status_code(login_response.status_code, HTTPStatus.OK)
-    assert_login_response(login_response_data)
+    validate_json_schema(response.json(), response_data.model_json_schema())
 
-    validate_json_schema(login_response.json(), login_response_data.model_json_schema())
